27a99964a3f0b427ec387fb2db1d5e4b542e9fec
PREM1808 <premkumar181716@gmail.com>
2026-04-20 21:56:09 +0530

    manage.py re-updated

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 import os
+import sys
 import pymysql
+
+# This MUST come before 'from django.core.wsgi import ...'
 pymysql.install_as_MySQLdb()
 
 from django.core.wsgi import get_wsgi_application
